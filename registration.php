3122d40a56164bba0c58c60396765199abbfd1b0
softinfoline <30929710+softinfoline@users.noreply.github.com>
2022-02-28 10:26:45 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Softinfoline Romanian Language Pack
+*/
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'softinfoline_ro_ro',
+    __DIR__
+);
